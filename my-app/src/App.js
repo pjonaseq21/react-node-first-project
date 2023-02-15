4d87c4b2a395b98770c9e6af0632b904b8aca58b
Gracjan Chamuła <gracjan.chamulati2019@gmail.com>
2023-02-15 22:17:09 +0100

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import React, { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Routes,Route, Link} from "react-router-dom";
+import Boxes from './routes/Boxes'
 
 import Admin from "./routes/Admin"
 import { View, StyleSheet } from 'react-native';
@@ -15,28 +16,12 @@ const styles = StyleSheet.create({
 function App() {
 
 
-  useEffect(() => {
-    fetch("http://localhost:3500/")
-    .then((response) =>response.json())
-    .then((data) =>  {
-      let test = document.getElementById("test")
-      let test1 = document.getElementById("test1")
-      let test2 = document.getElementById("test2")
-      console.log(data.result[0].email)
-      test.innerHTML = ` ${data.result[0].id} `;
-      test1.innerHTML = ` ${data.result[0].email} `;
-      test2.innerHTML = ` ${data.result[0].password} `;
-
-    }).catch(err => {
-    })
-    
-  },[]);
 
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-
+      
         <Route path="/admin" element={<Admin/>}></Route>
         <Route path='/users' element={<Users/>} />
 
--- my-app/src/routes/Boxes.js
+++ my-app/src/routes/Boxes.js
@@ -47,41 +47,37 @@ const styles = StyleSheet.create({
   });
 
 export default function Boxes() {
-  let [data, setData] = useState({})
-  let [dataSecond,setDataSecond] = useState([])
+  const [data, setData] = useState([]);
+  const [dataSecond, setDataSecond] = useState([]);
+
   useEffect(() => {
-    fetch(`http://localhost:3500/`)
-      .then((response) => response.json())
+    async function FetchData(){
+    await fetch(`http://localhost:3500/`)
+      .then((response) =>  response.json())
       .then((actualData) => {
-        setData(actualData.result);
-        setDataSecond(actualData.result[1]);
-      });
-  }, []);
-    return (
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <View style={styles.innerone} >
-          <Link to="/users">          
-          <span id="text-data">09-02-2023</span><br></br>
-          <span id="text"><strong>Najlepsze książki w 2023</strong></span>
-          </Link>
-          </View>                      
-        </View>
-        <View style={styles.box}>
-        
-        {Object.keys(data).map(function(index, value) {
-          console.log(data)
-          return(
-          <div key={index} style={{
-            width: "100%",
-            height: "100%",
-            backgroundImage: `url(${require(`../public/upload/${data.photo}`)})`
-          }}>
-            <span>TEST</span>
-          </div>
-         ) })
-        }
-        </View>
-      </View>
-    );
-  }
+        setData([actualData.result]);
+    });
+}FetchData()
+
+}, []);
+
+const boxes = data.map((item, index) => {
+  const photo = require(`../public/upload/${item.photo}`);
+
+  return (
+    <div
+      key={index}
+      style={{
+        width: "100%",
+        height: "500px",
+        backgroundImage: `url(${photo})`
+      }}
+    >
+      <span>TEST</span>
+    </div>
+  );
+});
+      return(
+        <div>{boxes}</div>
+      )
+    }
